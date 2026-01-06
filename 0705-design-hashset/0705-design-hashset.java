0e2533ee5aa866179f6928f0ed35fe1fc6a0b1bc
Sarvashakti Singh <singhsarvashakti970@gmail.com>
2026-01-07 00:43:42 +0530

    Time: 13 ms (78.23%), Space: 53.9 MB (25.89%) - LeetHub

---
+++ 0705-design-hashset/0705-design-hashset.java
@@ -0,0 +1,31 @@
+class MyHashSet {
+    private static final int MAX_SIZE= 1000001;
+    private boolean[] store;
+
+    public MyHashSet() {
+        store = new boolean[MAX_SIZE];
+        
+    }
+    
+    public void add(int key) {
+        store[key]=true;
+        
+    }
+    
+    public void remove(int key) {
+        store[key] = false;
+    }
+    
+    public boolean contains(int key) {
+        return store[key];
+        
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
