a305fd472d12027b8378cd0a8c071d572393b97c
Sarvashakti Singh <singhsarvashakti970@gmail.com>
2026-01-20 21:45:41 +0530

    Time: 4 ms (79.97%), Space: 94.5 MB (52.82%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -9,16 +9,47 @@
  * }
  */
 class Solution {
-    ListNode curr;
+
     public boolean isPalindrome(ListNode head) {
-        curr = head;
-        return solve(head);
+        if (head == null || head.next == null) {
+            return true;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        // Find middle
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // Reverse second half
+        ListNode secondHalf = reverseList(slow);
+        ListNode firstHalf = head;
+
+        // Compare
+        while (secondHalf != null) {
+            if (firstHalf.val != secondHalf.val) {
+                return false;
+            }
+            firstHalf = firstHalf.next;
+            secondHalf = secondHalf.next;
+        }
+
+        return true;
     }
 
-    public boolean solve(ListNode head) {
-        if(head == null) return true;
-        boolean ans = solve(head.next) && head.val == curr.val;
-        curr = curr.next;
-        return ans;
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
     }
 }
